7cf39eb75ea1cde91c325e0b7fde91aed8114528
damienbod <damien_bod@hotmail.com>
2014-08-19 22:46:04 +0200

    update

--- WebApiParameters/Controllers/ValuesController.cs
+++ WebApiParameters/Controllers/ValuesController.cs
@@ -6,28 +6,36 @@ namespace WebApiParameters.Controllers
 	[RoutePrefix("api/values")]
 	public class ValuesController : ApiController
 	{
-		// http://localhost:49407/api/values/par1?id=2
-		[Route("par1")]
+		// http://localhost:49407/api/values/example1?id=2
+		[Route("example1")]
 		[HttpGet]
 		public string Get(int id)
 		{
 			return "value";
 		}
 
-		// http://localhost:49407/api/values/par2?id1=1&id2=2&id3=3
-		[Route("par2")]
+		// http://localhost:49407/api/values/example2?id1=1&id2=2&id3=3
+		[Route("example2")]
 		[HttpGet]
 		public string GetWith3Parameters(int id1, long id2, double id3)
 		{
 			return "value";
 		}
 
-		// http://localhost:49407/api/values/par3/2/3/4
-		[Route("par4/{id1}/{id2}/{id3}")]
+		// http://localhost:49407/api/values/example3/2/3/4
+		[Route("example3/{id1}/{id2}/{id3}")]
 		[HttpGet]
 		public string GetWith3ParametersAttributeRouting(int id1, long id2, double id3)
 		{
 			return "value";
+		}
+
+		// http://localhost:49407/api/values/example4?id1=1&id2=2&id3=3
+		[Route("example4")]
+		[HttpGet]
+		public string GetWithUri([FromUri] ParamsObject paramsObject)
+		{
+			return "value:" + paramsObject.Id1;
 		}
 
 		// User-Agent: Fiddler
@@ -44,21 +52,15 @@ namespace WebApiParameters.Controllers
 		// Content-Type: application/json
 		//
 		// id1=1&id2=2&id3=3
-		// http://localhost:49407/api/values/par5
-		[Route("par5")]
+		// http://localhost:49407/api/values/example5
+		[Route("example5")]
 		[HttpPost]
 		public string GetWithBody([FromBody] ParamsObject paramsObject)
 		{
 			return "value:" + paramsObject.Id1;
 		}
 
-		// http://localhost:49407/api/values/par6?id1=1&id2=2&id3=3
-		[Route("par6")]
-		[HttpGet]
-		public string GetWithUri([FromUri] ParamsObject paramsObject)
-		{
-			return "value:" + paramsObject.Id1;
-		}
+		
 
 	}
 }
