0e26085b2789b64ceeb600e7962ee69db86b48bf
damienbod <damien_bod@hotmail.com>
2014-08-22 10:52:38 +0200

    Adding array examples

--- WebApiParameters/Controllers/ValuesController.cs
+++ WebApiParameters/Controllers/ValuesController.cs
@@ -10,9 +10,20 @@ namespace WebApiParameters.Controllers
 		// http://localhost:49407/api/values/example1?id=2
 		[Route("example1")]
 		[HttpGet]
-		public string Get(int id)
+		public List<List<ParamsObject>> Get(int id)
 		{
-			return "recieved: " + id;
+			var list = new List<List<ParamsObject>>();
+			
+			var paramsList = new List<ParamsObject>
+			{
+				new ParamsObject {Id1 = 3, Id2 = 76, Id3 = 19},
+				new ParamsObject {Id1 = 56, Id2 = 87, Id3 = 94},
+				new ParamsObject {Id1 = 976, Id2 = 345, Id3 = 7554}
+			};
+
+			list.Add(paramsList);
+			list.Add(paramsList);
+			return list;
 		}
 
 		// http://localhost:49407/api/values/example2?id1=1&id2=2&id3=3
@@ -68,11 +79,12 @@ namespace WebApiParameters.Controllers
 			return "value:" + paramsObject.Id1;
 		}
 
+		// http://localhost:49407/api/values/example6?paramsObject=2,paramsObject=4,paramsObject=9
 		[Route("example6")]
-		[HttpPost]
-		public string GetListWithBody([FromBody] List<int> paramsObject)
+		[HttpGet]
+		public string GetListFromUri([FromUri] List<int> paramsObject)
 		{
-			if (paramsObject != null && paramsObject.Count > 0)
+			if (paramsObject != null)
 			{
 				return "recieved a list with length:" + paramsObject.Count;
 			}
@@ -80,14 +92,36 @@ namespace WebApiParameters.Controllers
 			return "NOTHING RECIEVED...";
 		}
 
-		// http://localhost:49407/api/values/example7?paramsObject=2,paramsObject=4,paramsObject=9
+		// Content-Type: application/json
+		// [{"Id1":3,"Id2":76,"Id3":19},{"Id1":56,"Id2":87,"Id3":94},{"Id1":976,"Id2":345,"Id3":7554}]
+		//
+		// Content-Type: application/xml
+		// <ArrayOfParamsObject>
+        //   <ParamsObject><Id1>3</Id1><Id2>76</Id2><Id3>19</Id3></ParamsObject>
+        //   <ParamsObject><Id1>56</Id1><Id2>87</Id2><Id3>94</Id3></ParamsObject>
+        //   <ParamsObject><Id1>976</Id1><Id2>345</Id2><Id3>7554</Id3></ParamsObject>
+        // </ArrayOfParamsObject>
+		//
+		// http://localhost:49407/api/values/example7
 		[Route("example7")]
-		[HttpGet]
-		public string GetListFromUri([FromUri] List<int> paramsObject)
+		[HttpPost]
+		public string GetListFromBody([FromBody] List<ParamsObject> paramsList)
 		{
-			if (paramsObject != null)
+			if (paramsList != null)
 			{
-				return "recieved a list with length:" + paramsObject.Count;
+				return "recieved a list with length:" + paramsList.Count;
+			}
+
+			return "NOTHING RECIEVED...";
+		}
+
+		[Route("example8")]
+		[HttpPost]
+		public string GetListsFromBody([FromBody] List<List<ParamsObject>> paramsList)
+		{
+			if (paramsList != null)
+			{
+				return "recieved a list with length:" + paramsList.Count;
 			}
 
 			return "NOTHING RECIEVED...";
